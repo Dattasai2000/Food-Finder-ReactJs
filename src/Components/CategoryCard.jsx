45d07e08fdc5643acbe2ea0ebef013d74fccdf07
Dattasai <Dattasai2000>
2025-07-21 16:17:07 +0530

    commit message

--- src/Components/CategoryCard.jsx
+++ src/Components/CategoryCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export const CategoryCard = ({ category }) => (
   <Link to={`/category/${category.strCategory}`} className="block  rounded p-2 hover:shadow-md">
     <h3 className="text-center font-semibold mt-2 border w-28 bg-orange-500 ml-auto text-white rounded ">{category.strCategory}</h3>
-    <img src={category.strCategoryThumb} alt={category.strCategory} className="rounded shadow-md" />
+    <img src={category.strCategoryThumb} alt={category.strCategory} className="rounded " />
     
   </Link>
 );
